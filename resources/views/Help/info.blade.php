bc73de512a46e5a775b6fec85e7b5fcd237d417c
corentus12345 <corentin.brion@viacesi.fr>
2019-01-21 16:43:13 +0100

    information

--- resources/views/Help/info.blade.php
+++ resources/views/Help/info.blade.php
@@ -5,22 +5,23 @@
 @endsection
 
 @section('contenu')
-	<h1>Bureau des élèves</h1>
-	<h2>Qui sommes-nous ?</h2>
-	<p>Le BDE est un groupe de plusieurs étudiants de la première à la dernière année qui se rassemble régulièrement afin de proposer à ces derniers diverses activités. Ces derniers peuvent être proposés par les élèves dans la boîte à idées qui seront par la suite votées. <br />Une fois votée, c'est au BDE de mettre en place l'événement.</p>
-	<h2>Quelles activités pouvons nous proposer ? </h2>
-	<ul>
-		<li>Soirée étudiante</li>
-		<li>Voyages organisés</li>
-		<li>Échanges par correspondance</li>
-		<li>Tournoi sportif</li>
-		<li>Diverses activités secondaires</li>
-	</ul>
-	<p>Les membres du BDE sont élus à chaque début d'année scolaire par les étudiants afin de les représenter au cours de l'année.</p>
-	<h2>Qui sont les membres ? </h2>
-	//Membres à rajouter//
-	<h2>Où nous trouver ? </h2>
-	//Carte que je vais modifier d'ici Lundi normalement //
-	<p><i class="fas fa-map-marked"></i>19 Avenue Guy de Collongue 69130 Écully.</p>
-	<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=4.763777554035188%2C45.782466764985315%2C4.767318069934846%2C45.78417640398581&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=19/45.78332/4.76555">
+	<section id="infoGroup">
+		<h1 id="infoTittle">Bureau des élèves</h1>
+		<h2 class="secondTittle">Qui sommes-nous ?</h2>
+		<p class="infoTexte">Le BDE est un groupe de plusieurs étudiants de la première à la dernière année qui se rassemble régulièrement afin de proposer à ces derniers diverses activités. Ces derniers peuvent être proposés par les élèves dans la boîte à idées qui seront par la suite votées. <br />Une fois votée, c'est au BDE de mettre en place l'événement.</p>
+		<h2 class="secondTittle">Quelles activités pouvons nous proposer ? </h2>
+		<ul id="infoDot">
+			<li>Soirée étudiante</li>
+			<li>Voyages organisés</li>
+			<li>Échanges par correspondance</li>
+			<li>Tournoi sportif</li>
+			<li>Diverses activités secondaires</li>
+		</ul>
+		<p class="infoTexte">Les membres du BDE sont élus à chaque début d'année scolaire par les étudiants afin de les représenter au cours de l'année.</p>
+		<h2 class="secondTittle">Qui sont les membres ? </h2>
+		//Membres à rajouter//
+		<h2 class="secondTittle">Où nous trouver ? </h2>
+		<p class="infoTexte"><i class="fas fa-map-marked"></i>19 Avenue Guy de Collongue 69130 Écully.</p>
+		<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=4.763777554035188%2C45.782466764985315%2C4.767318069934846%2C45.78417640398581&amp;layer=mapnik" style="border: 1px solid black; margin-bottom: 30px;"></iframe><br/>
+	</section>
 @endsection
