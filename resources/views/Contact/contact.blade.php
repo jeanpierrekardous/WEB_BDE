18709d10676aea7bcd6bba6e8c3c9d1379c47fcc
corentus12345 <corentin.brion@viacesi.fr>
2019-01-22 10:44:53 +0100

    Update contact.blade.php

--- resources/views/Contact/contact.blade.php
+++ resources/views/Contact/contact.blade.php
@@ -6,14 +6,23 @@
 
 @section('contenu')
 	<section id="contactBDE">
+		<!-- Tittle -->
 		<h1 id="contactTittle">Nous contacter</h1>
-		{!! Form::open(['url' => 'contact', 'id' => 'formContact']) !!}
-			{!! Form::label('email', 'Votre adresse mail :') !!}
+		<!-- Open the form and add an id to the CSS file. -->
+		{!! Form::open(['url' => 'contact', 'id' => 'formContact']) !!} 
+			<!-- add a label 'email' with text and name for when we click on the text we go on the square. -->
+			{!! Form::label('email', 'Votre adresse mail :') !!} 
+			<!-- Square link with label 'email' if we write an email adress on this we send a mail with this adress mail. -->
 			{!! Form::email('email', null , array('autocomplete' => 'off', 'id' => 'email', 'placeholder' => 'Votre email', 'spellcheck' => 'false', 'required' => 'required')) !!}
+			<!-- add a label 'subjects' with text and name for when we click on the text we go on the square. -->
 			{!! Form::label('subject', 'Objet du mail :') !!}
+			<!-- Square link with label 'subject' if we write a subject  on this we send a mail with this subject. -->
         	{!! Form::text('subject', null , array('required' => 'required', 'id' => 'subject')) !!}
+        	<!-- add a label 'bodies' with text and name for when we click on the text we go on the square. -->
         	{!! Form::label('bodies', 'Message :') !!}
+        	<!-- Square link with label 'bodies' if we write a message on this we send a mail with this message. -->
         	{!! Form::textarea('bodies', null , array('required' => 'required', 'id' => 'contactMessage')) !!}
+        	<!-- create a button and send the mail with the information write in the square. -->
 			{!! Form::submit('Envoyer !', ['id' => 'submit']) !!}
         {!! Form::close() !!}
 	</section>
