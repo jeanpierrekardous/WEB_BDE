f768eafbbc488fe4c8cb38440fb6eecd1afeca71
corentus12345 <corentin.brion@viacesi.fr>
2019-01-22 10:27:07 +0100

    contactReceive

---
+++ resources/views/Contact/contactReceive.blade.php
@@ -0,0 +1,16 @@
+@extends('template')
+
+@section('tittle')
+	<title>Contact</title>
+@endsection
+
+@section('contenu')
+	<section id="contactBDE">
+		<h1 id="contactTittle">Nous contacter</h1>
+		<p>
+			Message envoyé rechargé la page si vous souhaité en envoyer un autre.<br>
+			Nous tacherons de vous répondre au plus vite. <br>
+			Bonne journée.	
+		</p>
+	</section>
+@endsection
