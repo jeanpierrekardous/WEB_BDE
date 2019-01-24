0ac046f88c0248bf2e370bb462a0d3cfc4097599
corentus12345 <corentin.brion@viacesi.fr>
2019-01-24 13:24:24 +0100

    php idea

---
+++ resources/views/Idea/writeIdea.blade.php
@@ -0,0 +1,20 @@
+@extends('template')
+
+@section('title')
+	<title>WriteIdea</title>
+@endsection
+
+@section('contenu')
+	<section id="contactBDE">
+		    <h1 id="contactTittle">Quel est l'événement que vous souhaité créer</h1>
+		    {!! Form::open(['url' => 'contact', 'id' => 'formContact']) !!} 
+		   		{!! Form::label('nameEvents', 'Nom de l'événement :') !!}
+		   		{!! Form::text('nameEvents', null , array('required' => 'required', 'id' => 'subject')) !!}
+		   		{!! Form::label('description', 'description de l'évenement :') !!}
+		   		{!! Form::text('description', null , array('required' => 'required', 'id' => 'contactMessage')) !!}
+		   		{!! Form::label('picture', 'Photos :') !!}
+		   		{!! Form::file('description', ['id' => 'contactMessage']) !!}
+		   		{!! Form::submit('Proposer', ['id' => 'submit']) !!}
+		   	{!! Form::close() !!}
+	</section>
+@endsection
