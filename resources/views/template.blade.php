c9dceba0160d9b7e5c9468f8b7a06cc0d7209124
corentus12345 <corentin.brion@viacesi.fr>
2019-01-24 10:30:56 +0100

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,6 +4,7 @@
     	<meta charset="utf-8">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<!-- Link to all .css -->
 		<link rel="stylesheet" type="text/css" href="css/home.css">
 		<link rel="stylesheet" type="text/css" href="css/general.css">
 		<link rel="stylesheet" type="text/css" href="css/contact.css">
