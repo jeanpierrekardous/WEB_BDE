6ffa6b05bd7752eadc9ab3849b75a50059961a7f
corentus12345 <corentin.brion@viacesi.fr>
2019-01-24 15:22:45 +0100

    Update wrongWriteIdea.blade.php

---
+++ resources/views/Idea/wrongWriteIdea.blade.php
@@ -0,0 +1,16 @@
+@extends('template')
+
+@section('title')
+<title>Boîtes à idées</title>
+@endsection
+
+@section('contenu')
+	<section id="contactBDE">
+		    <h1 id="contactTittle">Error</h1>
+		    <article id="formContact">
+		    	<p >
+		    		Erreur lors de la création d'une idée pour la boite à idée veuiller réessayer ultérieurement.
+		    	</p>
+		    </article>
+	</section>
+@endsection
