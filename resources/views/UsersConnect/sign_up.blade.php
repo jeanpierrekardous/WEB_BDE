cc7e6966713f97ab94169a5fefd712edbbc5bdf5
corentus12345 <corentin.brion@viacesi.fr>
2019-01-24 10:29:05 +0100

    Update sign_up.blade.php

--- resources/views/UsersConnect/sign_up.blade.php
+++ resources/views/UsersConnect/sign_up.blade.php
@@ -7,6 +7,7 @@
 @section('contenu')
 	<section id="signup">
 		<h1 id="titleSignup">Vous identifier</h1>
+		<!-- We open the form -->
 		{!! Form::open(['url' => 'signup', 'id' => 'signupForm']) !!}
 			{!! Form::label('firstname', 'Entrer votre prénom :') !!}
 			{!! Form::text('firstname', null, array('autocomplete' => 'off', 'id' => 'firstname', 'spellcheck' => 'false')) !!}
@@ -14,7 +15,9 @@
 			{!! Form::text('name', null, array('autocomplete' => 'off', 'id' => 'name', 'spellcheck' => 'false')) !!}
 			{!! Form::label('email', 'Entrer votre Email :') !!}
 			{!! Form::email('email', null , array('autocomplete' => 'off', 'id' => 'emailSignup', 'placeholder' => 'Votre email', 'spellcheck' => 'false')) !!}
+			<!-- open a drop down menu with different option -->
 			{!! Form::label('country', 'Quelle est votre centre ?') !!}
+			<!-- Differents option we can select -->
 			<select name="country" id="country">
 	           <option value="Lyon">Lyon</option>
 	           <option value="Paris">Paris</option>
@@ -27,11 +30,14 @@
        		<input type="password" name="password" id='password'>
        		{!! Form::label('password_confirmation', 'Vérification du mot de passe :') !!}
        		<input type="password" name="password_confirmation" id='password_confirmation'>
+       		<!-- Different error who can come -->
        		{!! $errors->first('password','<p class="help">:message</p>') !!}
        		{!! $errors->first('email','<p class="help">:message</p>') !!}
        		{!! $errors->first('name','<p class="help">:message</p>') !!}
        		{!! $errors->first('firstname','<p class="help">:message</p>') !!}
+
        		<?php
+       		// If all good he return to the first page else he return to the page sign up with the error write at the end. 
 				$URL = $_SERVER['HTTP_REFERER'];
 				if($URL == 'http://www.cesi-bde.com/signup' AND !$errors->first()){
 					?>
@@ -40,6 +46,7 @@
 				}
 			?>
 			{!! Form::submit('S\'inscrire !', ['id' => 'submitSignup']) !!}
+			<!-- We close the form -->
 		{!! Form::close() !!}
 	</section>
 @endsection
