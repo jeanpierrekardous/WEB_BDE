7ec4fbc2e648500f2eb59d01871af5eac51875cc
ElcocoSensei <corentin.bourgey@viacesi.fr>
2019-01-24 10:45:53 +0100

    ok

--- resources/views/Events/events.blade.php
+++ resources/views/Events/events.blade.php
@@ -1,12 +1,33 @@
 @extends('template')
 
 @section('title')
-<title>Évenements</title>
+	<title>Évenements</title>
 @endsection
 
 @section('contenu')
-<link rel="stylesheet" type="text/css" href="../css/events.css">
-<section id="list_events">
-	<h1>Liste des évènements</h1>
-	<article class="event"></article>
+	<section id="events">
+		<h1>Liste des évènements</h1>
+		<div id="listevents">
+			<article class="publication">
+				<h3 class="event_title">Titre</h3>
+				<img src="fonts/pictures/logo_test.png" class="test" alt="pictures_test">
+			</article>
+			<article class="publication">
+				<h3 class="event_title">Titre</h3>	
+				<img src="fonts/pictures/logo-mercedes.png" class="test" alt="test_2">
+			</article>
+			<article class="publication">
+				<h3 class="event_title">Titre</h3>
+				<img src="fonts/pictures/hitachi.png" class="test" alt="test_3">
+			</article>
+			<article class="publication">
+				<h3 class="event_title">Titre</h3>
+				<img src="fonts/pictures/nanosonde.png" class="test" alt="test_4">
+			</article>
+			<article class="publication">
+				<h3 class="event_title">Titre</h3>
+				<img src="fonts/pictures/bijuu.png" class="test" alt="test_5">
+			</article>
+		</div>
+	</section>
 @endsection
