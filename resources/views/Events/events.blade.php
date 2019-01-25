dc7ebebc0a702052d50b0649c96497699ea55070
corentus12345 <corentin.brion@viacesi.fr>
2019-01-25 10:08:59 +0100

    css

--- resources/views/Events/events.blade.php
+++ resources/views/Events/events.blade.php
@@ -5,29 +5,9 @@
 @endsection
 
 @section('contenu')
-	<section id="events">
-		<h1>Liste des évènements</h1>
-		<div id="listevents">
-			<article class="publication">
-				<h3 class="event_title">Titre</h3>
-				<img src="fonts/pictures/logo_test.png" class="test" alt="pictures_test">
-			</article>
-			<article class="publication">
-				<h3 class="event_title">Titre</h3>	
-				<img src="fonts/pictures/logo-mercedes.png" class="test" alt="test_2">
-			</article>
-			<article class="publication">
-				<h3 class="event_title">Titre</h3>
-				<img src="fonts/pictures/hitachi.png" class="test" alt="test_3">
-			</article>
-			<article class="publication">
-				<h3 class="event_title">Titre</h3>
-				<img src="fonts/pictures/nanosonde.png" class="test" alt="test_4">
-			</article>
-			<article class="publication">
-				<h3 class="event_title">Titre</h3>
-				<img src="fonts/pictures/bijuu.png" class="test" alt="test_5">
-			</article>
-		</div>
+	<section id="eventGroup">
+		<h1 id="eventTittle">Événements prévut</h1>
+		<a href="login" class="redirectLogin"><p class="connectNeed">Veuillez-vous connecter !</p></a>
+
 	</section>
 @endsection
