871565fecb9b44e51d826257b4f56d3f629731ce
corentus12345 <corentin.brion@viacesi.fr>
2019-01-28 13:53:14 +0100

    serveur node js

---
+++ public/nodeJs/nodeServeur.js
@@ -0,0 +1,25 @@
+//L'application requiert l'utilisation du module Express.
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.  
+var express = require('express');
+
+// Nous définissons ici les paramètres du serveur.
+var hostname = 'localhost'; 
+var port = 8888; 
+
+var app = express(); 
+
+var myRouter = express.Router(); 
+ 
+// Je vous rappelle notre route (/piscines).  
+myRouter.route('/projet/WEB_BDE/public/info')
+.get(function(req,res){ 
+	  res.json({message : "test", methode : req.method});
+})
+//show map of a fixed position
+
+app.use(myRouter);
+ 
+// Démarrer le serveur 
+app.listen(port, hostname, function(){
+	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port+"\n"); 
+});
