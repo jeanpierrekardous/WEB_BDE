c8f4a63bbf19bee696be906614ded56ff56896ba
corentus12345 <corentin.brion@viacesi.fr>
2019-01-28 10:51:19 +0100

    shop

--- resources/views/Shop/shop.blade.php
+++ resources/views/Shop/shop.blade.php
@@ -6,10 +6,15 @@
 
 @section('contenu')
 <section id="shopGroup">
-	<article class="shopSize">
-		<h2 id="shopTittle">Boutique du BDE</h2>
-		<img src="https://www.michel-edouard-leclerc.com/sites/default/files/2018-04/Coca-Cola%20E.Leclerc.jpg" class="articleShop"><a href=""><p>Ajouter au panier</p></a>
+	<h2 id="shopTittle">Boutique du BDE</h2>
+	<article id="shopSize">
+		<a href="panier" id="panierShop"><img src="https://us.123rf.com/450wm/jongjet303/jongjet3031703/jongjet303170300216/73540773-aper%C3%A7u-de-l-ic%C3%B4ne-du-panier-ic%C3%B4ne-pour-la-conception-du-site-web-logo.jpg?ver=6" alt="panier"></a>
 	</article>
+	<article id="info">
+        <a href="panier">
+            <img src="https://www.michel-edouard-leclerc.com/sites/default/files/2018-04/Coca-Cola%20E.Leclerc.jpg" class="picturesbody" alt="article1">
+            <h3 class="namecategoryShop">Ajouter au panier</h3>
+        </a>
 </section>
 
 @endsection
