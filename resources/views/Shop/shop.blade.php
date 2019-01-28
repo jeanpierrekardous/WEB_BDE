3a501803c58bc6ec7ac1d8de76073932ff924b69
corentus12345 <corentin.brion@viacesi.fr>
2019-01-28 10:30:52 +0100

    update css

--- resources/views/Shop/shop.blade.php
+++ resources/views/Shop/shop.blade.php
@@ -7,7 +7,8 @@
 @section('contenu')
 <section id="shopGroup">
 	<article class="shopSize">
-		<h1 id="shopTittle">Boutique du BDE</h1>
+		<h2 id="shopTittle">Boutique du BDE</h2>
+		<img src="https://www.michel-edouard-leclerc.com/sites/default/files/2018-04/Coca-Cola%20E.Leclerc.jpg" class="articleShop"><a href=""><p>Ajouter au panier</p></a>
 	</article>
 </section>
 
