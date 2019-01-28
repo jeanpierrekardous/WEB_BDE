1e508cec929e546f573ab9ed7e4f9df8fa9c7b66
corentus12345 <corentin.brion@viacesi.fr>
2019-01-28 13:55:11 +0100

    map

--- resources/views/Help/info.blade.php
+++ resources/views/Help/info.blade.php
@@ -37,8 +37,8 @@
 		<article class="infoSize">
 			<h2 class="secondTittle">Où nous trouver ? </h2>
 			<p class="infoTexte"><i class="fas fa-map-marked"></i>19 Avenue Guy de Collongue 69130 Écully.</p>
+			<iframe id="infoMap" src="https://www.openstreetmap.org/export/embed.html?bbox=4.7644293308258066%2C45.782666911429075%2C4.766939878463746%2C45.78397439248092&amp;layer=mapnik&amp;marker=45.78332065578796%2C4.765684604644775" style="border: 1px solid black; margin-bottom: 30px;"></iframe><br/>
 			<!-- api map -->
 		</article>
 	</section>
-	<script type="text/javascript" src="{{url('nodeJs/nodeServeur.js')}}">;(function map</script>
 @endsection
