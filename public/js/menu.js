16fb7c9c3c23034633a239408e4f98a6e485bbba
corentus12345 <corentin.brion@viacesi.fr>
2019-01-29 09:44:25 +0100

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,12 +8,12 @@ elementMenu.onclick = function() {
 		$("#flexboxmenuResponsive").css("display","flex").css("flex-direction","column").css("margin-top","3%");
 		$("#flexboxmenuResponsive").css("width","33%").css("position","absolute").css("top",$("header").height() - 40 + "px").css("background-color", "#000000").css("height","250px").css("right", "0").css("aligns-items", "center").css("justify-content","center");
 		$("#flexboxmenuResponsive li a").css("color", "white").css("text-decoration", "none").css("font-size", "30px").css("margin-top", "30px").css("padding", "5px");
-		$("#helpBurger").css("margin", "10px 0 10px 0");
-		$("#welcomeBurger").css("margin", "10px 0 10px 0").css("font-size", "30px");
-		$("#deconnexionBurger").css("margin", "10px 0 10px 0");
-		$("#adminBurger").css("margin", "10px 0 10px 0");
-		$("#connexionBurger").css("margin", "10px 0 10px 0");
-		$("#inscriptionBurger").css("margin", "10px 0 10px 0");
+		$("#helpBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
+		$("#welcomeBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
+		$("#deconnexionBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
+		$("#adminBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
+		$("#connexionBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
+		$("#inscriptionBurger").css("margin", "10px 0 10px 0").css("font-size", "30px").css("text-decoration", "none");
 
 		state = true;
 
